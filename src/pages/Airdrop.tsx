9baa18fdc637cc3c024abd46c7b80bf09cd34164
miki <euxddesigner@gmail.com>
2024-11-14 09:55:28 -0800

    Setup GitHubor deployment

--- src/pages/Airdrop.tsx
+++ src/pages/Airdrop.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 // import React from 'react'
 
 // import { TonConnectButton } from '@tonconnect/ui-react';
 // import { useTonConnect } from '@/hooks/useTonConnect.ts';
 // import { useCounterContract } from '@/hooks/useCounterContract';
 export default function Airdrop() {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    // Check if the Telegram Web App object is available
+    if (window.Telegram?.WebApp?.initDataUnsafe) {
+      // Retrieve the user's real Telegram username
+      const user = window.Telegram.WebApp.initDataUnsafe.user;
+      if (user) {
+        setUsername(user.username || `${user.first_name} ${user.last_name}`);
+      }
+    }
+  }, []);
 
   return (
     <div className='lmlm'>
@@ -11,9 +24,9 @@ export default function Airdrop() {
         
         {/* <TonConnectButton/> */}
         <div className='pp'>M</div>
-        <p>Mikiyas zenebe</p>
-        <p>Task Completed  <code>0</code></p>
-        <p>Invites <code>0</code></p>
+        <p>{username ? username : "Guest"}</p>
+        <p>Task Completed  <code>-</code></p>
+        <p>Invites <code>-</code></p>
         </div>
   )
 }
